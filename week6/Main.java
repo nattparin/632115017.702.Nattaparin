0395c6203087d4641702c8b4f362c0eaa5955c8f
Nattaparin Somnuek <nattaparin455@gmail.com>
2021-01-13 19:25:53 +0700

    week6

---
+++ week6/Main.java
@@ -0,0 +1,23 @@
+package week6;
+
+public class Main {
+    public static void main(String[] args) {
+        Artist art = new Artist("smart", 18,"male");
+
+        art.genre=" hip hop";
+        art.genre1="Engineer";
+        art.genre2="Doctor";
+        art.genre3="Programer";
+        art.genre4="Police";
+        art.genre5="Demon slayer";
+        
+        art.introduce();
+        art.playMusic();
+        art.Jobsung1();
+        art.Jobsung2();
+        art.Jobsung3();
+        art.Jobsung4();
+        art.Jobsung5();
+      
+    }
+}
