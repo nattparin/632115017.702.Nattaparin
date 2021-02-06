031bda515230ffeea515a85e0a597500378dc1e1
Nattaparin Somnuek <nattaparin455@gmail.com>
2021-02-06 17:44:43 +0700

    week3

--- week9/Rectangle.java
+++ week9/Rectangle.java
@@ -1,8 +1,8 @@
 package week9;
 
 public class Rectangle  extends Shape {
-    int width;
-    int height;
+    private int width;
+    private int height;
 
     public Rectangle( int width,int height) {
         this.width = width;
